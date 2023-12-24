c1d5ba6c8560bd1ac81b04ada8191cadc8a9a242
LeoArruda <lvarruda@gmail.com>
2023-12-23 22:54:33 -0500

    Add class Position

---
+++ Chess/Board/Position.cs
@@ -0,0 +1,27 @@
+﻿namespace Board
+{
+    internal class Position
+    {
+        public int row { get; set; }
+        public int column { get; set; }
+
+        public Position(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+
+        public void definirValores(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+
+        public override string ToString()
+        {
+            return row
+                + ", "
+                + column;
+        }
+    }
+}
