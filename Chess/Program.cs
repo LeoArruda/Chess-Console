091d8d8de7ddcd42b72e4e2d837f4eacdc9e8045
LeoArruda <lvarruda@gmail.com>
2023-12-24 00:24:08 -0500

    Started to implement a method to print out the Board

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using board;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace Chess
     {
         static void Main(string[] args)
         {
+            Board board = new Board(8,8);
+            Screen.printBoard(board);
+            Console.ReadLine();
         }
     }
 }
---
+++ Chess/Screen.cs
@@ -0,0 +1,27 @@
+﻿using board;
+using System;
+
+namespace Chess
+{
+    internal class Screen
+    {
+        public static void printBoard(Board board)
+        {
+            for (int i = 0; i < board.rows; i++)
+            {
+                for (int j = 0; j < board.columns; j++)
+                {
+                    if (board.piece(i, j) != null)
+                    {
+                        Console.Write(board.piece(i, j) + " ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
